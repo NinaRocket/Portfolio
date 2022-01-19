bc36d7b0091f8f7ca98395d7f7f2a378ed0b7f3c
NRocket <ninamrocket@gmail.com>
2022-01-18 19:13:56 -0700

    Updates

---
+++ precache-manifest.a7d4c316b7ad4cbe8afbaa5b6e183ec1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2153de30d11010dc9d8dab90e028b34",
+    "url": "/%7BPortfolio%7D/index.html"
+  },
+  {
+    "revision": "0d0cec238483ea877eb4",
+    "url": "/%7BPortfolio%7D/static/css/main.570a342c.chunk.css"
+  },
+  {
+    "revision": "fad64209249e96fde520",
+    "url": "/%7BPortfolio%7D/static/js/2.e607f15b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/%7BPortfolio%7D/static/js/2.e607f15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0cec238483ea877eb4",
+    "url": "/%7BPortfolio%7D/static/js/main.8fe21791.chunk.js"
+  },
+  {
+    "revision": "c984011e039e5b76ab45",
+    "url": "/%7BPortfolio%7D/static/js/runtime-main.f707b660.js"
+  },
+  {
+    "revision": "931c5090a29efa486cd1c39e08de8080",
+    "url": "/%7BPortfolio%7D/static/media/NRocketPic.931c5090.jpg"
+  },
+  {
+    "revision": "39da1586cbf217b62f417f80f0a1553f",
+    "url": "/%7BPortfolio%7D/static/media/eatdaburger.39da1586.jpg"
+  },
+  {
+    "revision": "fdbf3e7d5342b5a30a474391168648b0",
+    "url": "/%7BPortfolio%7D/static/media/redicon.fdbf3e7d.svg"
+  }
+]);
